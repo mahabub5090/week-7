07b73ba6c04bac2c7833f1a6d90d4d294fe5b40f
mahabub5090 <mdmahabub.rahman789@gmail.com>
2023-11-20 20:05:03 +0600

    first commit

---
+++ Favourite_Numbers.cpp
@@ -0,0 +1,48 @@
+//@ Problem Link: https://www.codechef.com/problems/FAVOURITENUM
+
+/*
+**************************************************************
+*Bis'Millah Hir Rah'Manir Rahim == start with the name of \n *
+*                    ALLAH Subhana Tayala                    *
+*                Author: Md. Mahabubar Rahman                *
+*               CSE,RPI && Rangpur, Bangladesh               *
+**************************************************************
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define Allhamdulillah 0
+#define pb(x) push_back(x)
+#define pp() pop_back()
+#define ll long long int
+#define foi(i, s, e) for (int i = s; i < e; i++)
+#define cn(x) cin >> x
+#define endl '\n'
+#define Endl '\n'
+#define nl cout << '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    (!(n % 2) && !(n % 7)) ? cout << "Alice\n" : (n % 2 && !(n % 9)) ? cout << "Bob\n"
+                                                                     : cout << "Charlie\n";
+}
+/*mdmahabub55*/
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    // t = 1;
+
+    while (t--)
+    {
+        solve();
+    }
+    return Allhamdulillah;
+}
